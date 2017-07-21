0e14f3bc1ed0cc49ecdb577436ee1aef7e557fab
x4storm <x4storm@gmail.com>
2017-07-21 15:22:51 +0800

    update Fix for this filename: angular-datatables.light-columnfilter.a.js
    beacause the gulp-rev always prepend hash in first dots.
    so reg should consider after .-

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 var through = require('through2');
 
-module.exports = function(ver) {
-    var ver = ver || 'ver'
-    // convert a-xxxxxxxx.css to a.css?ver=xxxxxxxx
-    function hashToQuery(file) {
-        var content = new String(file.contents);
-        var manifestObj = JSON.parse(content)
-        for(var key in manifestObj){
-            manifestObj[key] = manifestObj[key].replace(/(\-(\w+))((\.\w+)+)/g, function($, $1, $2, $3) {
-                return $3 + '?' + ver + '=' + $2;
-            })
-        }
-        file.contents = new Buffer(JSON.stringify(manifestObj, null, '\t'));
-        file.ver = ver;
-        return file;
+module.exports = function (ver) {
+  var ver = ver || 'ver'
+  // convert a-xxxxxxxx.css to a.css?ver=xxxxxxxx
+  function hashToQuery(file) {
+    var content = new String(file.contents);
+    var manifestObj = JSON.parse(content)
+    for (var key in manifestObj) {
+      manifestObj[key] = manifestObj[key].replace(/(\-(\w+))((\.[-\w]+)+)/g, function ($, $1, $2, $3) {
+        return $3 + '?' + ver + '=' + $2;
+      })
     }
-    return through.obj(function(file, encoding, callback) {
-        callback(null, hashToQuery(file));
-    });
+    file.contents = new Buffer(JSON.stringify(manifestObj, null, '\t'));
+    file.ver = ver;
+    return file;
+  }
+  return through.obj(function (file, encoding, callback) {
+    callback(null, hashToQuery(file));
+  });
 };
